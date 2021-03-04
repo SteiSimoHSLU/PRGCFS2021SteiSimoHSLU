502187fc6fa39ac3acf120ef1458b97927ed3040
prgc <simon.steinegger@stud.hslu.ch>
2021-03-04 19:37:08 +0100

    test

---
+++ 40_Exercises/03_Datatypes/datatypes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	printf( "size of char: %ld byte(s).\n", sizeof(char) );
+	printf( "size of char: %ld byte(s).\n", sizeof(unsigned char) );
+	printf( "size of char: %ld byte(s).\n", sizeof(short) );
+	printf( "size of char: %ld byte(s).\n", sizeof(int) );
+				
+
+
+	return 0;
+}
