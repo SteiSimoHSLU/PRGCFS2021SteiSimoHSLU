76d07345a74b83f11dbcc57125c50bb9a5be99ba
prgc <simon.steinegger@stud.hslu.ch>
2021-03-18 20:33:32 +0100

    asdf

--- 40_Exercises/04_CLA_Operators/logicOperators.c
+++ 40_Exercises/04_CLA_Operators/logicOperators.c
@@ -1,18 +1,81 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-	//falls NICHT 3 Argumente, dann Block 7-10
-	if( argc != 3)
+	//falls NICHT 4 Argumente, dann Block 7-10
+	if( argc != 4)
 	{
 		//input not valid
 		printf("Usage:\n");
-		printf("logicOperators Intput 1 Intput 2\n");
+		printf("logicOperators Intput1 Input2 Input3\n");
 	}
 	else 
 	{
-		//input valid -> genau drei Argumente erhalten
+		//input valid -> genau 1+drei Argumente erhalten
+		int val1 = atoi(argv[1]);
+		int val2 = atoi(argv[2]);		
+		int val3 = atoi(argv[3]);
+		int sum = 0;
+		float average = 0.0f;
+		
+		printf("%d, %d, %d\n", val1, val2, val3);
+		
+		if( !(val1 > 0) || !(val2 > 0) || !(val3 > 0))
+		{
+			//fehlerfall
+			printf("All input must be greater than zero\n");
+		}
+		else
+		{	
+			//find greatest
+			if ( val1 >= val2 && val1 >=val3)
+			{
+				//val1 
+				printf("%d is the greatest\n", val1);
+			}
+			else if  (val2 >= val1 && val2 >=val3)
+			{
+				//val 
+				printf("%d is the greatest\n", val2);
+			}
+			else
+			{
+				printf("%d is the greatest\n", val3);	
+			}
+			
+			//find smallest
+			if ( val1 <= val2 && val1 <=val3)
+			{
+				//val1 
+				printf("%d is the smallest\n", val1);
+			}
+			else if  (val2 <= val1 && val2 <=val3)
+			{
+				//val 
+				printf("%d is the smallest\n", val2);
+			}
+			else
+			{
+				printf("%d is the smallest\n", val3);	
+			}
+			
+			//sum
+			sum = (val1+val2+val3);
+			printf("the sum is: %d\n", sum );
+			
+			//avg
+			average= sum/3.0;
+			printf("the average is: %.2f\n", average );
+		}
+
+			
+	}
+		
+#if 0 		// auskommentierem mit Präprozessor		
+		
 		printf("Number of input arguments: %d\n", argc ); // Anzahl args printen
+		
 		for(int i = 0; i < argc; i++) 
 		//nimm int i, starte mit 0, dann mach
 		//führe folgendes aus, solange i < argc
@@ -27,5 +90,6 @@ int main(int argc, char *argv[])
 			//		- zweite Zahl
 		}
 	}
+#endif	
 	return 0;
 }
--- 40_Exercises/WS04_Schleifen/loopExercise.c
+++ 40_Exercises/WS04_Schleifen/loopExercise.c
@@ -2,18 +2,89 @@
 
 int main(int argc, char *argv[])
 {
-	int sum = 0;
-	int start = 1;
-	int end = 100;
+
+//2. even numbers from 0-10
+	int sum1 = 0;
+	int start1 = 2;
+	int end1 = 10;
 	
-	for( int i = start; i <=end; i++)
+	for( int i1 = start1; i1 <=end1;i1=i1+2)
 	{
-		sum=sum+ i;
+		
+		sum1=sum1+ i1;
+		printf("%d \n",i1);
 	
 	}
+	printf("The sum from %d to %d is equal to %d\n\n", start1, end1, sum1);
+	
+//3. 40-25	
+	int start2 = 40;
+	int end2 = 25;
+	
+	for( int i2 = start2; i2 >=end2;i2=i2-1)
+	{
+		printf("%d \n",i2);
+	
+	}
+	printf("\n");
+	
+	
+	//4. hoch	
+	int start3 = 1;
+	int end3 = 800;
+	
+	for( int i3 = start3; i3 <=end3; )
+	{
+		printf("%d\n",i3);
+		i3=i3*3;
+	}
+	
 
-	printf("The sum from %d to %d is equal to %d\n", start, end, sum);
-
+//5. Alle nummern von 0-100
+	int start4 =1;
+	int end4 =10;
+	int out =0;
+	
+	for( int i4 = 0; i4 <=9; i4=i4+1 )
+	{
+		for (int i5 =1; i5<=10; )
+		{
+			out = 10*i4+i5;
+			printf("%d ",out);
+			i5=i5+1;
+		}
+		printf("\n");
+		
+	}
+	
+	printf("\n");
+	
+//6. break
+	int start6 =1;
+	int end6 =1000;
+	int limit =555;
+	int sum6 =0;
+	int uprCounter = 0;
+	int sum7 =0;
+	
+	
+	for( int i6 = start6; i6 <=end6;  )
+	{
+		sum6=sum6+i6;
+		
+		
+		if (sum6>=limit )
+		{
+			break;
+		}
+		sum7 =sum6;
+		uprCounter = i6;
+		i6=i6+1;
+		
+	}
+	printf("with max. sum %d, the sum of the integeres from %d to %d is %d \n ",limit, start6, uprCounter, sum7  );
 	return 0;
+	
+	
 }
 
