54939bc0bc9d8db451216b4824afabe1755a0f7e
prgc <simon.steinegger@stud.hslu.ch>
2021-05-06 19:29:28 +0200

    SW10

---
+++ 30_Testat/binConverter.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct oneLine
+{
+   long long timeStamp;
+   int pressure;
+   char systemState;
+   char alarmState;
+};
+
+struct oneLine allData[10];
+
+int main(int argc, char *argv[])
+{
+	FILE *inputFile=./;
+	FILE *outputFile;
+	
+	
+
+
+
+	return 0;
+}
+
