1f1ce084e43d12e9febf441d68934c82888eb72b
prgc <simon.steinegger@stud.hslu.ch>
2021-05-08 17:50:43 +0200

    asdf

--- 30_Testat/binConverter.c
+++ 30_Testat/binConverter.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+FILE *fptrInput;
+FILE *fptrOgccutput;
+	
 struct oneLine
 {
    long long timeStamp;
@@ -13,13 +17,46 @@ struct oneLine allData[10];
 
 int main(int argc, char *argv[])
 {
-	FILE *inputFile=./;
-	FILE *outputFile;
+	// READ
+	fptrInput=fopen("./pressureSpike.bin","rb");
+	// Check if valid
+	if (fptrInput == NULL){
+		printf("Error when opening file\n");
+
+		// Program exits if the file pointer returns NULL.
+		exit(1);
+		}
+	//console output
+	char buff[255];
+	for (int i=0; i<100; i++)
+		{
+			int a=0;
+			int b=0;
+			int c=0;
+			int d=0;
+			
+			//read long long, 64 bit
+			fgets(buff, 255, fptrInput);
+			a = (int) buff;
+			printf("%d\n", a );
+			/*
+			//read long long 24 bit
+			fscanf(fptrInput, "%s", buff);
+			b = (int) buff;
+			printf("b%d ", a );
+			//read long long 8 bit
+			fscanf(fptrInput, "%s", buff);
+			c = (int) buff;
+			printf("c%d ", a );
+			//read long long 8 bit
+			fscanf(fptrInput, "%s", buff);
+			d = (int) buff;
+			printf("d%d\n", a );
+			*/
+		
+		}
 	
 	
-
-
-
 	return 0;
 }
 
